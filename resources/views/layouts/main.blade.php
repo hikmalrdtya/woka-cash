a9e63000c6c3f65df568f62e411b303e10fd2cb7
hikmalrdtya <hikmalraditya299@gmail.com>
2025-11-20 21:01:44 +0700

    first commit

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="favicon.ico">
+    <script>
+        if (localStorage.getItem("dark-mode") === "true") {
+            document.documentElement.classList.add("dark");
+        }
+    </script>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <title>@yield('title')</title>
+</head>
+
+<body>
+    <div class="flex min-h-screen">
+        {{-- Sidebar --}}
+        @include('layouts.sidebar')
+
+        {{-- Main Content --}}
+        <main class="page-content">
+            @yield('content')
+        </main>
+    </div>
+    <script src="{{ asset('js/bundle.js') }}"></script>
+</body>
+
+</html>
