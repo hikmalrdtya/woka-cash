e159b4cbabe3c12cae7d0fe1589d5e65748a402a
naufalmaulanaaa86-ux <naufalmaulanaaa86@gmail.com>
2025-11-26 10:57:32 +0700

    edit profile

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -508,7 +508,7 @@ $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(valu
                                 <ul
                                     class="flex flex-col gap-1 border-b border-gray-200 pt-4 pb-3 dark:border-gray-800">
                                     <li>
-                                        <a href="profile.html"
+                                        <a href="{{ route('admin.profile.edit')}}"
                                             class="group text-theme-sm flex items-center gap-3 rounded-lg px-3 py-2 font-medium text-gray-700 hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300">
                                             <svg class="fill-gray-500 group-hover:fill-gray-700 dark:fill-gray-400 dark:group-hover:fill-gray-300"
                                                 width="24" height="24" viewBox="0 0 24 24" fill="none"
--- routes/web.php
+++ routes/web.php
@@ -15,21 +15,23 @@ Route::get('/', action: function () {
 
 
 Route::middleware('guest')->group(function () {
-    Route::get('/login',[AuthController::class,'showLogin'])->name('login');
-    Route::post('/login',[AuthController::class,'login'])->name('login.post');
+    Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+    Route::post('/login', [AuthController::class, 'login'])->name('login.post');
 });
 
-Route::post('/logout',[AuthController::class,'logout'])->name('logout')->middleware('auth');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
 
 Route::prefix('admin')->name('admin.')->middleware(['auth', 'role:admin'])->group(function () {
-   Route::get('/dashboard',[DashboardController::class,'index'])->name('dashboard');
-   Route::resource('user', UserController::class);
-   Route::resource('branch', BranchController::class);
-   Route::resource('branchUser', BranchUserController::class);
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('user', UserController::class);
+    Route::resource('branch', BranchController::class);
+    Route::resource('branchUser', BranchUserController::class);
+    Route::get('/profile', [UserController::class, 'editProfile'])->name('profile.edit');
+    Route::put('/profile', [UserController::class, 'updateProfile'])->name('profile.update');
 });
 
 Route::prefix('staff')->name('staff.')->middleware(['auth', 'role:staff'])->group(function () {
-   Route::get('/dashboard',[DashboardController::class,'index'])->name('dashboard');
-   Route::resource('incomes', IncomesController::class);
-   Route::resource('expenses', ExpensesController::class);
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('incomes', IncomesController::class);
+    Route::resource('expenses', ExpensesController::class);
 });
